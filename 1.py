d063fc6261c5dfb48ae9bcf939a87aacf4bd6fb2
ivandanielobregon <ivandanielobregon@gmail.com>
2026-04-06 22:14:47 -0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -36,27 +36,3 @@ mi_nota_mas_alta = calcular_nota_mas_alta(mis_notas)
 print(f"La nota más alta es: {mi_nota_mas_alta}")
 mi_nota_mas_baja = calcular_nota_mas_baja(mis_notas)
 print(f"La nota más baja es: {mi_nota_mas_baja}")
-
-#Segundo punto
-def filtrar_aprobados(estudiantes):
-    estudiantes_aprobados = [] 
-    for nombre, nota in estudiantes: 
-        if nota >= 3.0: 
-            estudiantes_aprobados.append((nombre, nota)) 
-    return estudiantes_aprobados
-lista_estudiantes = [
-    ("Ana", 4.5),
-    ("Luis", 2.8),
-    ("María", 3.0),
-    ("Carlos", 1.5),
-    ("Sofía", 4.0)
-]
-estudiantes_que_aprobaron = filtrar_aprobados(lista_estudiantes)
-print("Lista original de estudiantes:", lista_estudiantes)
-print("Estudiantes aprobados:", estudiantes_que_aprobaron)
-print("\nProbando con una lista vacía de estudiantes:")
-print(filtrar_aprobados([]))
-estudiantes_que_aprobaron = filtrar_aprobados(lista_estudiantes)
-
-print("Lista original de estudiantes:", lista_estudiantes)
-print("Estudiantes aprobados:", estudiantes_que_aprobaron)
